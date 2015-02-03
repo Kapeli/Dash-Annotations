d88785913673138dca4a19c38ad64b267e7ac2c0
Kapeli <support@kapeli.com>
2015-02-03 08:50:31 +0200

    Remove Workbench

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -57,19 +57,3 @@ Patchwork\Utf8\Bootup::initMbstring();
 */
 
 Illuminate\Support\ClassLoader::register();
-
-/*
-|--------------------------------------------------------------------------
-| Register The Workbench Loaders
-|--------------------------------------------------------------------------
-|
-| The Laravel workbench provides a convenient place to develop packages
-| when working locally. However we will need to load in the Composer
-| auto-load files for the packages so that these can be used here.
-|
-*/
-
-if (is_dir($workbench = __DIR__.'/../workbench'))
-{
-	Illuminate\Workbench\Starter::start($workbench);
-}
