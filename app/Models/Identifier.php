bf938c0c00d78076763ccef4ddf6cd06a0220916
Kapeli <support@kapeli.com>
2015-02-14 06:15:31 +0200

    This should work

--- app/Models/Identifier.php
+++ app/Models/Identifier.php
@@ -36,11 +36,11 @@ class Identifier extends Eloquent {
         
         $page_path = $this->page_path;
         $basename = basename($page_path);
+        $basename = str_replace(['-2.html', '-3.html', '-4.html', '-5.html', '-6.html', '-7.html', '-8.html', '-9.html'], '.html', $basename);
         $page_path = substr($page_path, 0, strlen($page_path)-strlen($basename));
         $page_path = preg_replace('/[0-9]+\.*[0-9]+(\.*[0-9]+)*/', '', $page_path); // remove versions
         $page_path = preg_replace('/[0-9]+_*[0-9]+(_*[0-9]+)*/', '', $page_path); // remove retarded versions that use _ instead of . (SQLAlchemy!)
         $page_path = str_replace(range(0,9), '', $page_path); // remove all numbers
-        $page_path = str_replace('-.html', '.html', $page_path);
         $page_path = remove_prefix("www.", $page_path); // remove "www." for online-cloned docsets
         $page_path = trim(str_replace('//', '/', $page_path));
         $page_path .= $basename;
