e633b23daa0d89b69b67885450c00d8f3be6d5e0
Kapeli <support@kapeli.com>
2015-06-21 06:02:01 +0300

    Add charset declaration to annotation HTML

--- app/Http/Controllers/EntriesController.php
+++ app/Http/Controllers/EntriesController.php
@@ -432,6 +432,7 @@ class EntriesController extends Controller {
             <html lang="en">
                 <head>
                     <title>".$entry->title."</title>
+                    <meta charset="utf-8" />
                     <meta name="viewport" content="width=device-width, initial-scale=1">
                     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
                     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
