243fd2d3c9a130b8d2236e696a813ea88d4b019b
Kapeli <support@kapeli.com>
2015-07-24 06:15:38 +0300

    Trim apple docset names in the right place

--- app/Models/Identifier.php
+++ app/Models/Identifier.php
@@ -40,6 +40,32 @@ class Identifier extends Eloquent {
 
     public function trim()
     {
+        $docset_filename = $this->docset_filename;
+        $docset_filename = preg_replace('/\\.docset$/', '', $docset_filename);
+        $docset_filename = preg_replace('/[0-9]+\.*[0-9]+(\.*[0-9]+)*/', '', $docset_filename); // remove versions
+        $docset_filename = trim(str_replace(range(0,9),'',$docset_filename)); // remove all numbers
+        $this->docset_filename = $docset_filename;
+        $this->trim_apple_docset_names();
+
+        $page_path = $this->page_path;
+        $page_path = str_replace("https://", "http://", $page_path);
+        $page_path = str_replace("swiftdoc.org/swift-2/", "swiftdoc.org/", $page_path);
+        $basename = basename($page_path);
+        $page_path = substr($page_path, 0, strlen($page_path)-strlen($basename));
+        $basename = str_replace(['-2.html', '-3.html', '-4.html', '-5.html', '-6.html', '-7.html', '-8.html', '-9.html'], '.html', $basename);
+        $page_path = preg_replace('/v[0-9]+\.*[0-9]+(\.*[0-9]+)*/', '', $page_path); // remove versions like /v1.1.0/
+        $page_path = preg_replace('/[0-9]+\.*[0-9]+(\.*[0-9]+)*/', '', $page_path); // remove versions like /1.1.0/
+        $page_path = preg_replace('/[0-9]+_*[0-9]+(_*[0-9]+)*/', '', $page_path); // remove versions that use _ instead of . (SQLAlchemy)
+        $page_path = str_replace(range(0,9), '', $page_path); // remove all numbers
+        $page_path = str_replace(['/-alpha/', '/-alpha./', '/-alpha-/', '/-beta/', '/-beta./', '/-beta-/', '/-rc/', '/-rc./', '/-rc-/', '/.alpha/', '/.alpha./', '/.alpha-/', '/.beta/', '/.beta./', '/.beta-/', '/.rc/', '/.rc./', '/.rc-/'], '/', $page_path);
+        $page_path = remove_prefix($page_path, "www."); // remove "www." for online-cloned docsets
+        $page_path = trim(str_replace('//', '/', $page_path));
+        $page_path .= $basename;
+        $this->page_path = $page_path;
+    }
+
+    public function trim_apple_docset_names()
+    {
         if($this->docset_filename == 'prerelease')
         {
             if(has_prefix($this->page_path, 'ios/'))
@@ -69,28 +95,6 @@ class Identifier extends Eloquent {
         {
             $this->docset_filename = "com.apple.adc.documentation.iOS";
         }
-
-        $docset_filename = $this->docset_filename;
-        $docset_filename = preg_replace('/\\.docset$/', '', $docset_filename);
-        $docset_filename = preg_replace('/[0-9]+\.*[0-9]+(\.*[0-9]+)*/', '', $docset_filename); // remove versions
-        $docset_filename = trim(str_replace(range(0,9),'',$docset_filename)); // remove all numbers
-        $this->docset_filename = $docset_filename;
-        
-        $page_path = $this->page_path;
-        $page_path = str_replace("https://", "http://", $page_path);
-        $page_path = str_replace("swiftdoc.org/swift-2/", "swiftdoc.org/", $page_path);
-        $basename = basename($page_path);
-        $page_path = substr($page_path, 0, strlen($page_path)-strlen($basename));
-        $basename = str_replace(['-2.html', '-3.html', '-4.html', '-5.html', '-6.html', '-7.html', '-8.html', '-9.html'], '.html', $basename);
-        $page_path = preg_replace('/v[0-9]+\.*[0-9]+(\.*[0-9]+)*/', '', $page_path); // remove versions like /v1.1.0/
-        $page_path = preg_replace('/[0-9]+\.*[0-9]+(\.*[0-9]+)*/', '', $page_path); // remove versions like /1.1.0/
-        $page_path = preg_replace('/[0-9]+_*[0-9]+(_*[0-9]+)*/', '', $page_path); // remove versions that use _ instead of . (SQLAlchemy)
-        $page_path = str_replace(range(0,9), '', $page_path); // remove all numbers
-        $page_path = str_replace(['/-alpha/', '/-alpha./', '/-alpha-/', '/-beta/', '/-beta./', '/-beta-/', '/-rc/', '/-rc./', '/-rc-/', '/.alpha/', '/.alpha./', '/.alpha-/', '/.beta/', '/.beta./', '/.beta-/', '/.rc/', '/.rc./', '/.rc-/'], '/', $page_path);
-        $page_path = remove_prefix($page_path, "www."); // remove "www." for online-cloned docsets
-        $page_path = trim(str_replace('//', '/', $page_path));
-        $page_path .= $basename;
-        $this->page_path = $page_path;
     }
 
     public function find_in_db()
