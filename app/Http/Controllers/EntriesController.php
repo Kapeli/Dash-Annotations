c9154b1507482e56780d9c2c5ea2578f1b8c9eb4
Kapeli <support@kapeli.com>
2016-10-21 19:03:04 +0300

    Use SSL for css

--- app/Http/Controllers/EntriesController.php
+++ app/Http/Controllers/EntriesController.php
@@ -441,8 +441,8 @@ class EntriesController extends Controller {
                     <title>".$entry->title."</title>
                     <meta charset="utf-8" />
                     <meta name="viewport" content="width=device-width, initial-scale=1">
-                    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-                    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+                    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
                     <style>
                         code {
                             color: black;
