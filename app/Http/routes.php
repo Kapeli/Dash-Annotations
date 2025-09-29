d7282ad31943776fa051defa8963ac0a2157d6f5
Kapeli <support@kapeli.com>
2025-09-29 19:21:26 +0300

    Lumen 10.x changes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,32 +1,34 @@
 <?php
 
-$app->post('users/register', 'App\Http\Controllers\UsersController@register');
-$app->post('users/login', 'App\Http\Controllers\UsersController@login');
-$app->get('users/logout', 'App\Http\Controllers\UsersController@logout');
-$app->post('users/email', 'App\Http\Controllers\UsersController@change_email');
-$app->post('users/password', 'App\Http\Controllers\UsersController@change_password');
-$app->post('users/forgot/request', 'App\Http\Controllers\ForgotController@request');
-$app->post('users/forgot/reset', 'App\Http\Controllers\ForgotController@reset');
+/** @var \Laravel\Lumen\Routing\Router $router */
 
-$app->post('teams/create', 'App\Http\Controllers\TeamsController@create');
-$app->post('teams/join', 'App\Http\Controllers\TeamsController@join');
-$app->post('teams/leave', 'App\Http\Controllers\TeamsController@leave');
-$app->post('teams/set_role', 'App\Http\Controllers\TeamsController@set_role');
-$app->post('teams/remove_member', 'App\Http\Controllers\TeamsController@remove_member');
-$app->post('teams/set_access_key', 'App\Http\Controllers\TeamsController@set_access_key');
-$app->post('teams/list', 'App\Http\Controllers\TeamsController@list_teams');
-$app->post('teams/list_members', 'App\Http\Controllers\TeamsController@list_members');
+$router->post('users/register', 'App\Http\Controllers\UsersController@register');
+$router->post('users/login', 'App\Http\Controllers\UsersController@login');
+$router->get('users/logout', 'App\Http\Controllers\UsersController@logout');
+$router->post('users/email', 'App\Http\Controllers\UsersController@change_email');
+$router->post('users/password', 'App\Http\Controllers\UsersController@change_password');
+$router->post('users/forgot/request', 'App\Http\Controllers\ForgotController@request');
+$router->post('users/forgot/reset', 'App\Http\Controllers\ForgotController@reset');
 
-$app->post('entries/list', 'App\Http\Controllers\EntriesController@list_entries');
-$app->post('entries/save', 'App\Http\Controllers\EntriesController@save');
-$app->post('entries/create', 'App\Http\Controllers\EntriesController@save');
-$app->post('entries/get', 'App\Http\Controllers\EntriesController@get');
-$app->post('entries/vote', 'App\Http\Controllers\EntriesController@vote');
-$app->post('entries/delete', 'App\Http\Controllers\EntriesController@delete');
-$app->post('entries/remove_from_public', 'App\Http\Controllers\EntriesController@remove_from_public');
-$app->post('entries/remove_from_teams', 'App\Http\Controllers\EntriesController@remove_from_teams');
+$router->post('teams/create', 'App\Http\Controllers\TeamsController@create');
+$router->post('teams/join', 'App\Http\Controllers\TeamsController@join');
+$router->post('teams/leave', 'App\Http\Controllers\TeamsController@leave');
+$router->post('teams/set_role', 'App\Http\Controllers\TeamsController@set_role');
+$router->post('teams/remove_member', 'App\Http\Controllers\TeamsController@remove_member');
+$router->post('teams/set_access_key', 'App\Http\Controllers\TeamsController@set_access_key');
+$router->post('teams/list', 'App\Http\Controllers\TeamsController@list_teams');
+$router->post('teams/list_members', 'App\Http\Controllers\TeamsController@list_members');
 
-$app->get('/', function()
+$router->post('entries/list', 'App\Http\Controllers\EntriesController@list_entries');
+$router->post('entries/save', 'App\Http\Controllers\EntriesController@save');
+$router->post('entries/create', 'App\Http\Controllers\EntriesController@save');
+$router->post('entries/get', 'App\Http\Controllers\EntriesController@get');
+$router->post('entries/vote', 'App\Http\Controllers\EntriesController@vote');
+$router->post('entries/delete', 'App\Http\Controllers\EntriesController@delete');
+$router->post('entries/remove_from_public', 'App\Http\Controllers\EntriesController@remove_from_public');
+$router->post('entries/remove_from_teams', 'App\Http\Controllers\EntriesController@remove_from_teams');
+
+$router->get('/', function() use ($router)
 {
     return "Nothing to see here. Move along.";
 });
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,11 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+// Enable all error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->load();
 
