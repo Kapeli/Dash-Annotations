b155c8cdda7a245712312efd7f032c37cb1feed6
Kapeli <support@kapeli.com>
2025-09-29 18:44:56 +0300

    Support PHP 8.3

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -19,10 +19,10 @@ class Handler extends ExceptionHandler {
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $e
+     * @param  \Throwable  $e
      * @return void
      */
-    public function report(Exception $e)
+    public function report(\Throwable $e)
     {
         return parent::report($e);
     }
@@ -31,10 +31,10 @@ class Handler extends ExceptionHandler {
      * Render an exception into an HTTP response.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
+     * @param  \Throwable  $e
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $e)
+    public function render($request, \Throwable $e)
     {
         return parent::render($request, $e);
     }
